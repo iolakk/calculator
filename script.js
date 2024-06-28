7682d769b98e8970219a2eafc4f0247f38b4573b
iolakk <170725865+iolakk@users.noreply.github.com>
2024-06-28 16:52:46 +0200

    Add operate function that does the equation with specified operator on two numbers

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ let add = (a, b) => a + b;
 let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(a, b, operator){
+    let result;
+    switch(operator){
+        case '+':
+            result = add(a, b);
+            break;
+        case '-':
+            result = subtract(a, b);
+            break;
+        case '*':
+            result = multiply(a, b);
+            break;
+        case '/':
+            result = divide(a, b);
+            break;
+    }
+    
+    return result;
+}
